c9b65745c094630db01d58d9912656fc6c26cfb2
Aoi Emerauda <aoi@emerauda.com>
2021-09-05 10:20:01 +0900

    some fix

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ async function resync(interaction) {
     console.log(StreamKey + " is stopped!");
     while (player.state.status === "idle") {
         await sleep(5000);
-        if (connection.state.networking.state.connectionData.speaking) {
+        if (!connection.state.networking.state.connectionData.speaking) {
             const resource = createAudioResource(url,
                 {
                     inputType: StreamType.Arbitrary,
