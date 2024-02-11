bd264aeb5e697c85676dcc260480f55396560b0c
ROZ a.k.a. Aoi Emerauda <aoi@emerauda.com>
2024-02-11 22:06:40 +0900

    for discord.js 14

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const { Client } = require("discord.js");
-const { joinVoiceChannel, entersState, VoiceConnectionStatus, createAudioResource, StreamType, createAudioPlayer, AudioPlayerStatus, NoSubscriberBehavior, generateDependencyReport } = require("@discordjs/voice");
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, AudioPlayerStatus, generateDependencyReport } = require('@discordjs/voice');
 const { token } = require('./config.json');
-const client = new Client({ intents: ['GUILDS', 'GUILD_VOICE_STATES'] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 console.log(generateDependencyReport());
-const subscriptions = new Map();
 function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
-let StreamKey = null;
-let connection = null;
-let url = null;
+let streamKey = null;
+let streamUrl = null;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! client ready...`);
@@ -19,65 +17,32 @@ client.on('ready', () => {
 // play command
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand() || !interaction.guildId) return;
-    let subscription = subscriptions.get(interaction.guildId);
     if (interaction.commandName === "play") {
         await interaction.deferReply();
-        const guild = interaction.guild;
-        const member = await guild.members.fetch(interaction.member.id);
-        const memberVC = member.voice.channel;
-        if (!memberVC) {
-            return interaction.editReply({
-                content: "Voice channel is not found to connect.",
-                ephemeral: true,
-            });
+        const voiceChannel = interaction.member.voice.channel;
+        if (!interaction.member.voice.channelId) {
+          await interaction.reply('You need to be in a voice channel to use this command!');
+          return;
         }
-        if (!memberVC.joinable) {
-            return interaction.editReply({
-                content: "Voice channel is inaccessible.",
-                ephemeral: true,
-            });
-        }
-        if (!memberVC.speakable) {
-            return interaction.editReply({
-                content: "Bot do not have permission to play audio in Voice channel.",
-                ephemeral: true,
-            });
-        }
-        const status = ["●Loading Sounds...", `●Connecting to ${memberVC}...`];
-        const p = interaction.editReply(status.join("\n"));
         if (!connection) {
             connection = joinVoiceChannel({
-                guildId: guild.id,
-                channelId: memberVC.id,
-                adapterCreator: guild.voiceAdapterCreator,
-                selfMute: false,
+              channelId: voiceChannel.id,
+              guildId: interaction.guildId,
+              adapterCreator: interaction.guild.voiceAdapterCreator,
             });
         };
         // Extract the stream URL from the command
-        StreamKey = interaction.options.get('streamkey').value;
-        url = "rtsp://topaz.chat/live/" + StreamKey;
-        const resource = createAudioResource(url,
-            {
-                inputType: StreamType.Arbitrary,
-            });
-        const player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-        player.play(resource, { highWaterMark: 1024 * 1024 * 50, volume: false });
-        console.log(StreamKey + " is playing!");
-        const promises = [];
-        promises.push(entersState(connection, VoiceConnectionStatus.Ready, 1000 * 10).then(() => status[0] += "Done!"));
-        promises.push(entersState(player, AudioPlayerStatus.AutoPaused, 1000 * 10).then(() => status[1] += "Done!"));
-        await Promise.race(promises);
-        await p;
-        await Promise.all([...promises, interaction.editReply(status.join("\n"))]);
+        streamKey = interaction.options.get('streamkey').value;
+        streamUrl = `rtsp://topaz.chat/live/${streamKey}`;
+        const resource = createAudioResource(streamUrl);
+        const player = createAudioPlayer();
+        player.play(resource);
+        console.log(`${streamKey} is playing!`);
         connection.subscribe(player);
         await entersState(player, AudioPlayerStatus.Playing, 5000);
-        await interaction.editReply("Playing " + StreamKey);
-        await entersState(player, AudioPlayerStatus.Idle, 2 ** 31 - 1);
-        console.log(StreamKey + " is stopped!");
+        await interaction.editReply(`Playing ${streamKey}`);
+        await entersState(player, AudioPlayerStatus.Idle);
+        console.log(`${streamKey} is stopped!`);
         // Automatic disconnection if no stream is detected for 30 minutes
         let lastActivityTime = new Date();
         while (player.state.status === "idle") {
@@ -88,29 +53,22 @@ client.on('interactionCreate', async (interaction) => {
             }
             if (connection.state.subscription.player._state.status === "idle") {
 
-                const resource = createAudioResource(url,
-                    {
-                        inputType: StreamType.Arbitrary,
-                    });
-                const player = createAudioPlayer({
-                    behaviors: {
-                        noSubscriber: NoSubscriberBehavior.Pause,
-                    },
-                });
+              const resource = createAudioResource(streamUrl);
+              const player = createAudioPlayer();
                 await sleep(3000);
-                console.log(StreamKey + " is autoresuming...");
-                player.play(resource, { highWaterMark: 1024 * 1024 * 50, volume: false });
+                console.log(`${streamKey} is autoresuming...`);
+                player.play(resource);
                 connection.subscribe(player)
                 await sleep(5000);
                 if (connection.state.status === "destroyed") {
                     break;
                 }
                 if (connection.state.subscription.player._state.status === "playing") {
-                    console.log(StreamKey + " is autoresumed!");
+                    console.log(`${streamKey} is autoresumed!`);
                     let lastActivityTime = new Date();
                 }
                 if (currentTime - lastActivityTime > 1800000) {
-                    console.log(StreamKey + " is autodestroyed!");
+                    console.log(`${streamKey} is autodestroyed!`);
                     connection.destroy();
                     break;
                 }
@@ -119,9 +77,8 @@ client.on('interactionCreate', async (interaction) => {
         }
     }
     return {
-        connection: connection,
-        StreamKey: StreamKey,
-        url: url,
+        streamKey: streamKey,
+        streamUrl: streamUrl,
     };
 })
 
@@ -129,64 +86,31 @@ client.on('interactionCreate', async (interaction) => {
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand() || !interaction.guildId) return;
     let connection = null;
-    let subscription = subscriptions.get(interaction.guildId);
     if (interaction.commandName === "resync") {
         await interaction.deferReply();
-        const guild = interaction.guild;
-        const member = await guild.members.fetch(interaction.member.id);
-        const memberVC = member.voice.channel;
-        if (!memberVC) {
-            return interaction.editReply({
-                content: "Voice channel is not found to connect.",
-                ephemeral: true,
-            });
+        const voiceChannel = interaction.member.voice.channel;
+        if (!interaction.member.voice.channelId) {
+          await interaction.reply('You need to be in a voice channel to use this command!');
+          return;
         }
-        if (!memberVC.joinable) {
-            return interaction.editReply({
-                content: "Voice channel is inaccessible.",
-                ephemeral: true,
-            });
-        }
-        if (!memberVC.speakable) {
-            return interaction.editReply({
-                content: "Bot do not have permission to play audio in Voice channel.",
-                ephemeral: true,
-            });
-        }
-        const status = ["●Reloading Sounds...", `●Reconnecting to ${memberVC}...`];
-        const p = interaction.editReply(status.join("\n"));
         if (!connection) {
             connection = joinVoiceChannel({
-                guildId: guild.id,
-                channelId: memberVC.id,
-                adapterCreator: guild.voiceAdapterCreator,
-                selfMute: false,
+              channelId: voiceChannel.id,
+              guildId: interaction.guildId,
+              adapterCreator: interaction.guild.voiceAdapterCreator,
             });
         };
-        const url = "rtsp://topaz.chat/live/" + StreamKey;
-        const resource = createAudioResource(url,
-            {
-                inputType: StreamType.Arbitrary,
-            });
-        const player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-        console.log(StreamKey + " is resyncing...");
-        player.play(resource, { highWaterMark: 1024 * 1024 * 50, volume: false });
-        console.log(StreamKey + " is playing!");
-        const promises = [];
-        promises.push(entersState(connection, VoiceConnectionStatus.Ready, 1000 * 10).then(() => status[0] += "Done!"));
-        promises.push(entersState(player, AudioPlayerStatus.AutoPaused, 1000 * 10).then(() => status[1] += "Done!"));
-        await Promise.race(promises);
-        await p;
-        await Promise.all([...promises, interaction.editReply(status.join("\n"))]);
+        const streamUrl = `rtsp://topaz.chat/live/${streamKey}`;
+        const resource = createAudioResource(streamUrl);
+        const player = createAudioPlayer();
+        console.log(`${streamKey} is resyncing...`);
+        player.play(resource);
+        console.log(`${streamKey} is playing!`);
         connection.subscribe(player);
         await entersState(player, AudioPlayerStatus.Playing, 5000);
-        await interaction.editReply("Playing " + StreamKey);
-        await entersState(player, AudioPlayerStatus.Idle, 2 ** 31 - 1);
-        console.log(StreamKey + " is stopped!");
+        await interaction.editReply(`Playing ${streamKey}`);
+        await entersState(player, AudioPlayerStatus.Idle);
+        console.log(`${streamKey} is stopped!`);
         // Automatic disconnection if no stream is detected for 30 minutes
         let lastActivityTime = new Date();
         while (player.state.status === "idle") {
@@ -197,29 +121,22 @@ client.on('interactionCreate', async (interaction) => {
             }
             if (connection.state.subscription.player._state.status === "idle") {
 
-                const resource = createAudioResource(url,
-                    {
-                        inputType: StreamType.Arbitrary,
-                    });
-                const player = createAudioPlayer({
-                    behaviors: {
-                        noSubscriber: NoSubscriberBehavior.Pause,
-                    },
-                });
+                const resource = createAudioResource(streamUrl);
+                const player = createAudioPlayer();
                 await sleep(3000);
-                console.log(StreamKey + " is autoresuming...");
-                player.play(resource, { highWaterMark: 1024 * 1024 * 50, volume: false });
+                console.log(`${streamKey} is autoresuming...`);
+                player.play(resource);
                 connection.subscribe(player)
                 await sleep(5000);
                 if (connection.state.status === "destroyed") {
                     break;
                 }
                 if (connection.state.subscription.player._state.status === "playing") {
-                    console.log(StreamKey + " is autoresumed!");
+                    console.log(`${streamKey} is autoresumed!`);
                     let lastActivityTime = new Date();
                 }
                 if (currentTime - lastActivityTime > 1800000) {
-                    console.log(StreamKey + " is autodestroyed!");
+                    console.log(`${streamKey} is autodestroyed!`);
                     connection.destroy();
                     break;
                 }
@@ -233,33 +150,22 @@ client.on('interactionCreate', async (interaction) => {
 // stop command
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand() || !interaction.guildId) return;
-    let subscription = subscriptions.get(interaction.guildId);
     if (interaction.commandName === "stop") {
         await interaction.deferReply();
-        const player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause,
-            },
-        });
-        const guild = interaction.guild;
-        const member = await guild.members.fetch(interaction.member.id);
-        const memberVC = member.voice.channel;
-        const status = ["●Stopping Sounds...", `●Disconnecting from ${memberVC}...`];
-        const p = interaction.editReply(status.join("\n"));
+        const voiceChannel = interaction.member.voice.channel;
+        if (!interaction.member.voice.channelId) {
+          await interaction.reply('You need to be in a voice channel to use this command!');
+          return;
+        }
+        const player = createAudioPlayer();
         if (!connection) {
             connection = joinVoiceChannel({
-                guildId: guild.id,
-                channelId: memberVC.id,
-                adapterCreator: guild.voiceAdapterCreator,
-                selfMute: false,
+              channelId: voiceChannel.id,
+              guildId: interaction.guildId,
+              adapterCreator: interaction.guild.voiceAdapterCreator,
             });
         };
-        const promises = [];
-        promises.push(entersState(connection, VoiceConnectionStatus.Ready, 1000 * 10).then(() => status[0] += "Done!"));
-        promises.push(entersState(player, AudioPlayerStatus.AutoPaused, 1000 * 10).then(() => status[1] += "Done!"));
-        await Promise.race(promises);
-        await p;
-        console.log(StreamKey + " is destroyed!");
+        console.log(`${streamKey} is destroyed!`);
         connection.destroy();
         await interaction.editReply("Destroyed");
     };
