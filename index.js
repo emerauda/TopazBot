aba9cf70492b525a34f102cf2fecfbdc597ce34c
Aoi Emerauda <mail@mofumofu.me>
2024-12-06 12:09:13 +0900

    Fix variable declaration for lastActivityTime in index.js to ensure proper scoping during stream activity logging

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on('interactionCreate', async (interaction) => {
                 }
                 if (connection.state.subscription.player._state.status === "playing") {
                     console.log(`${streamKey} is autoresumed!`);
-                    let lastActivityTime = new Date();
+                    lastActivityTime = new Date();
                 }
                 if (currentTime - lastActivityTime > 1800000) {
                     console.log(`${streamKey} is autodestroyed!`);
